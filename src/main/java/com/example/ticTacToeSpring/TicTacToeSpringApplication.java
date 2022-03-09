0b5d840f5b06a8edc65e9779d83bea99ac1510a1
settilorenzo <settilorenzo97@gmail.com>
2022-03-09 14:28:24 +0100

    first commit

---
+++ src/main/java/com/example/ticTacToeSpring/TicTacToeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticTacToeSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ticTacToeSpring/TicTacToeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticTacToeSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
